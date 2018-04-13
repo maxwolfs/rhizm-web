fea06756bd2ce028a33c22a7258b920a0a0445b8
Maximilian Wolfs <max.wolfs@floc.de>
2018-04-13 18:10:44 +0200

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,29 @@
 from flask import Flask
+from datetime import datetime
+from pony.orm import *
 
 app = Flask(__name__)
+
+db = Database()
+
+class Intercourse(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    timestamp = Required(datetime)
+    first = Required(str)
+    second = Required(str)
+    verified = Optional(bool)
+
+
+class Card(db.Entity):
+    id = PrimaryKey(str)
+    password = Required(str)
+
+# PostgreSQL for Deployment on Heroku
+# db.bind(provider='postgres', user='', password='', host='', database='')
+
+# SQLite for local Development
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+
+db.generate_mapping(create_tables=True)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
 from app import app
+from app import db
+from pony.orm import *
+from app import Card, Intercourse
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/api/v1/intercourse', methods=['POST','GET'])
+def intercourse():
+    content = request.get_json()
+    return "bla"
+
