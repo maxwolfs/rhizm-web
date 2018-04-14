81c90d974bfa81d0529d08fabbadd052e3a29e46
Maximilian Wolfs <max.wolfs@floc.de>
2018-04-14 22:37:09 +0200

    Update

--- app/routes.py
+++ app/routes.py
@@ -4,29 +4,35 @@ from pony.orm import *
 from app import Card, Intercourse
 from flask import request, jsonify, abort
 from hashlib import blake2b
+from datetime import datetime
 
 # {'my_id' : '123456', 'secret_key' : '123456', 'other_id' : '1234567'}
+
+# curl -d '{ 'my_id': 'f049cd234b5a445766394925a2f0c46961d526f2', 'secret_key': 'genesis', 'other_id': 'e62ed43fffb6b96168089033be8efb65937c62a8', }' -H "Content-Type: application/json" -X POST http://localhost:5000/api/v1/intercourse
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World!"
 
-@app.route('/api/v1/intercourse', methods=['POST','GET'])
+
+@app.route('/api/v1/intercourse', methods=['POST'])
+@db_session
 def intercourse():
     data = request.get_json()
     myid = data['my_id']
     otherid = data['other_id']
+    p = data['secret_key'].encode('utf-8')
     c = Card[data['my_id']]
     h = blake2b(digest_size=20)
-    # if c not None and c.password == h.update(data['secret_key']).hexdigest():
-    #     intercourses = select(i for i in Intercourse if i.second == myid and i.first == otherid and if not i.verified == False)
-    #     if intercourses:
-    #         for i in Intercourse: 
-    #             i.verified = True
-    #     else 
-    #         Intercourse(timestamp = datetime(),first = myid, second = otherid, verified = False)
-    # else 
-    #     return abort(403)
-    # commit()   
+    d = h.update(p)
+    if c is not None and c.password == h.hexdigest():
+        intercourses = select(i for i in Intercourse if i.second == myid and i.first == otherid and i.verified == False)
+        if intercourses:
+            for i in Intercourse: i.verified = True
+        else: 
+            Intercourse(timestamp = datetime.utcnow(),first = myid, second = otherid, verified = False)
+    else: 
+        return abort(403)
+    commit()   
     return jsonify(data)
