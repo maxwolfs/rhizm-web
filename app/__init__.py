af228727f356252d08f12cc8afe53743e216f7e8
Maximilian Wolfs <max.wolfs@floc.de>
2018-04-15 19:06:43 +0200

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ class Card(db.Entity):
     password = Required(str)
 
 # PostgreSQL for Deployment on Heroku
-db.bind(provider='postgres', user='xacnpsztnthsnn', password='5e405b46617563293d9a313ff94c47542cf4df1e7db5e4d1f225e023939f61f6', host='ec2-79-125-110-209.eu-west-1.compute.amazonaws.com', database='dccridt2qs7hjp')
+# db.bind(provider='postgres', user='xacnpsztnthsnn', password='5e405b46617563293d9a313ff94c47542cf4df1e7db5e4d1f225e023939f61f6', host='ec2-79-125-110-209.eu-west-1.compute.amazonaws.com', database='dccridt2qs7hjp')
 
 # SQLite for local Development
 # db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
