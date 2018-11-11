91a0eeade9d74ec38d20cb332c41e43b6bc1cf95
Maximilian Wolfs <max.wolfs@homevoice.io>
2018-11-11 17:20:05 +0100

    update this happened

--- app/static/js/graph.js
+++ app/static/js/graph.js
@@ -308,12 +308,12 @@ function getCountOfObjectInList(obj, list) {
   return count;
 }
 
-addEventListener("click", function() {
-  var el = document.documentElement,
-    rfs =
-      el.requestFullscreen ||
-      el.webkitRequestFullScreen ||
-      el.mozRequestFullScreen ||
-      el.msRequestFullscreen;
-  rfs.call(el);
-});
+// addEventListener("click", function() {
+//   var el = document.documentElement,
+//     rfs =
+//       el.requestFullscreen ||
+//       el.webkitRequestFullScreen ||
+//       el.mozRequestFullScreen ||
+//       el.msRequestFullscreen;
+//   rfs.call(el);
+// });
